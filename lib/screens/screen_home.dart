504d99d6494a7f46fbc6fe91833041661411a749
vishnuraj-m <acappella.studiov@gmail.com>
2022-09-06 17:03:57 +0530

    Single Single Tap

---
+++ lib/screens/screen_home.dart
@@ -0,0 +1,54 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ScreenHome extends StatefulWidget {
+  const ScreenHome({super.key});
+
+  @override
+  State<ScreenHome> createState() => _ScreenHomeState();
+}
+
+class _ScreenHomeState extends State<ScreenHome> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: const Text('Dicee'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+          child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                  });
+                  print("left button pressed");
+                },
+                child: Image.asset('assets/images/dice$leftDiceNumber.png'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+                  print("right button pressed");
+                },
+                child: Image.asset('assets/images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
